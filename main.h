b6f49f042e4254b62c4a6d448e1c6df832aac454
bmokase3093 <bmokase3093@gmail.com>
2022-04-19 04:35:40 -0700

    The function that produces output accodring to a format

--- main.h
+++ main.h
@@ -1,5 +1,23 @@
 #ifndef MAIN_F
 #define MAIN_F
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
 /* Declare function prototypes */
 int _printf(const char *format, ...);
 #endif
