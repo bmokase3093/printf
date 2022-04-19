c2f9af1df40ba1fc53cb0018702cbbcca6c3bc24
bmokase3093 <bmokase3093@gmail.com>
2022-04-19 06:57:17 -0700

    Write a function that produces output according to a format.

--- basic_op.c
+++ basic_op.c
@@ -17,6 +17,7 @@ int op_char(va_list p)
 	return (1);
 }
 
+
 /**
  * op_string - main function
  * @p: The argument pointer.
@@ -45,6 +46,7 @@ int op_string(va_list p)
 	return (i);
 }
 
+
 /**
  * op_percent - main function
  * @p: The argument pointer.
@@ -57,4 +59,49 @@ int op_percent(va_list __attribute__((unused)) p)
 {
 	_putchar('%');
 	return (1);
+}
+
+
+/**
+ * op_numbers - main function
+ * @arg: The argument pointer.
+ *
+ * Description: This function prints a:
+ * Integrers.
+ * Decimals.
+ *
+ * Return: 0.
+ */
+int op_numbers(va_list arg)
+{
+	unsigned int i, p, r;
+
+	int n = va_arg(arg, int), count = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	p = 1;
+	r = i;
+
+	while (r > 9)
+	{
+		p *= 10;
+		r /= 10;
+	}
+
+	for (; p >= 1; p /= 10)
+	{
+		_putchar(((i / p) % 10) + '0');
+		count++;
+	}
+	return (count);
 }
