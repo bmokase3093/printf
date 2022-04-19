02fd58d3c62235166f275bf7aeee903969a9b6ad
bmokase3093 <bmokase3093@gmail.com>
2022-04-19 05:58:47 -0700

    Write a function that produces output according to a format.

--- main.h
+++ main.h
@@ -1,46 +1,34 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
+/* Libraries to be used */
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
 
 /**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
+ * struct base- struct token
+ *
+ * @op: The identifier argument
+ * @f: The base function
+ */
+typedef struct  base
 {
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+	char *op;
+	int (*f)(va_list p);
+} base_t;
 
-/*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
+/* Funtion prototypes */
 int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
+int selector(int *i, const char *format, va_list x);
 
-/*Helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
+/* Basic task Prototypes */
+int op_char(va_list p);
+int op_string(va_list p);
+int op_percent(va_list __attribute__((unused)) p);
 
-
+/* Additional Prototypes */
+int _putchar(char c);
 #endif
