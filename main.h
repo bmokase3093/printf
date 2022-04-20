1b953c42a77b9defef1d495ede484dac66026673
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-04-19 19:52:32 -0700

    write a function that produces output according to a format

--- main.h
+++ main.h
@@ -1,77 +1,44 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H_
+#define _PRINTF_H_
 
+/* Libraries */
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
 
 /**
- * struct flags - struct containing flags to "turn on"
- * when a flag specifier is passed to _printf()
- * @plus: flag for the '+' character
- * @space: flag for the ' ' character
- * @hash: flag for the '#' character
+ *  * struct base- struct token.
+ *   *
+ *    * @op: The identifier argument.
+ *     * @f: The base function.
  */
-typedef struct flags
+typedef struct base
 {
-    int plus;
-    int space;
-    int hash;
-} flags_t;
+	char *op;
+	int (*f)(va_list p);
+} base_t;
 
-/**
- * struct printHandler - struct to choose the right function depending
- * on the format specifier passed to _printf()
- * @c: format specifier
- * @f: pointer to the correct printing function
-i */
-typedef struct printHandler
-{
-    char c;
-    int (*f)(va_list ap, flags_t *f);
-} ph;
 
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-
-/* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-
-/* _printf */
+/* General Prototypes */
 int _printf(const char *format, ...);
+int selector(int *i, const char *format, va_list x);
 
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
+/* Basic Prototypes */
+int op_char(va_list p);
+int op_string(va_list p);
+int op_percent(va_list __attribute__((unused)) p);
+int op_numbers(va_list arg);
 
-/* get_flag */
-int get_flag(char s, flags_t *f);
+/* Advanced Prototypes*/
+int op_unsigned(va_list arg);
+int op_octal(va_list p);
+int op_binary(va_list p);
+int print_rev(va_list p);
+int rot13(va_list p);
 
-/* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-
-/* write_funcs */
+/* Auxiliar Prototypes */
 int _putchar(char c);
-int _puts(char *str);
-
-/* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
-
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
-/* print_percent */
-int print_percent(va_list l, flags_t *f);
-
 
 #endif
